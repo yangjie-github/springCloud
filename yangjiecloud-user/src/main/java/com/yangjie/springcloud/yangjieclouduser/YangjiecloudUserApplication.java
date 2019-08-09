aa6c2b7a80d3bf659fb91722260320b436924240
yangjie <18202975766@163.com>
2019-08-09 22:14:07 +0800

    添加eureka服务，并完成服务注册（单个服务）

--- yangjiecloud-user/src/main/java/com/yangjie/springcloud/yangjieclouduser/YangjiecloudUserApplication.java
+++ yangjiecloud-user/src/main/java/com/yangjie/springcloud/yangjieclouduser/YangjiecloudUserApplication.java
@@ -3,10 +3,15 @@ package com.yangjie.springcloud.yangjieclouduser;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-//proxyTargetClass = true 不加会报错
+/**
+ * @EnableTransactionManagement(proxyTargetClass = true) 事务管理，proxyTargetClass = true 不加会报错
+ * @EnableEurekaClient 会注册进euruka服务
+ */
 
+@EnableEurekaClient
 @EnableTransactionManagement(proxyTargetClass = true)
 @SpringBootApplication
 @MapperScan("com.yangjie.springcloud.yangjiecloudapi.dao")
