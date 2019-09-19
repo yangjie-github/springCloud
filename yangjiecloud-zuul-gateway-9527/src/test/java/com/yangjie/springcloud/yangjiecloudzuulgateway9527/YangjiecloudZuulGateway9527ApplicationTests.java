72eabc5136cc8d9d9b24f7e1ed64fde0a09fd125
yangjie <18202975766@163.com>
2019-09-19 12:22:30 +0800

    完成zuul整合

---
+++ yangjiecloud-zuul-gateway-9527/src/test/java/com/yangjie/springcloud/yangjiecloudzuulgateway9527/YangjiecloudZuulGateway9527ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yangjie.springcloud.yangjiecloudzuulgateway9527;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YangjiecloudZuulGateway9527ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
